2e3e2456d1ac79cff54c8ccbbd58d9ab05268ddb
abdulrahman <abdulrahmanibnimohammed@gmail.com>
2025-04-21 12:21:59 +0300

    confirmed if the asssignment was done

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom';
-
 import App from "../App";
 
-// Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+  // Arrange
+  // Act
+  // Assert
+});
